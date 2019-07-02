19c775f576cb17d670a9e46c71b04f7f4cfccf03
anitha1987 <asaminathan@unomaha.edu>
2019-07-02 15:17:44 -0500

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>Hello Instructor this is an about page, You will be viewing Anitha Saminathan's Skill set in the home page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
